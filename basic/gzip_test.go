d90d7f0ed33b4ffb774525cba6028e2668f164d5
by46 <ycs_ctbu_2010@126.com>
2018-05-05 08:22:16 +0800

    feat: add more example

--- basic/gzip_test.go
+++ basic/gzip_test.go
@@ -3,6 +3,7 @@ package basic
 import (
 	"archive/tar"
 	"bytes"
+	"compress/bzip2"
 	"compress/gzip"
 	"fmt"
 	"io"
@@ -92,4 +93,41 @@ func TestUnTar(t *testing.T) {
 		}
 	}
 
+}
+
+func TestUnBzip(t *testing.T) {
+	content, err := ioutil.ReadFile("../im.tar.bz2")
+	assert.Nil(t, err)
+	mimeType := http.DetectContentType(content)
+	fmt.Printf("mime type: %s\n", mimeType)
+
+	kind, _ := filetype.Match(content)
+	fmt.Printf("extension %s  MIME %s\n", kind.Extension, kind.MIME.Value)
+	bzipReader := bzip2.NewReader(bytes.NewBuffer(content))
+
+	reader := tar.NewReader(bzipReader)
+
+	for {
+		header, err := reader.Next()
+		switch {
+		case err == io.EOF:
+			fmt.Print("EOF")
+			return
+		case err != nil:
+			fmt.Print("Error")
+			return
+		case header == nil:
+			continue
+		}
+
+		switch header.Typeflag {
+		case tar.TypeReg:
+			buf := bytes.NewBuffer(nil)
+			io.Copy(buf, reader)
+			fmt.Printf("filename: %s %d\n", header.Name, buf.Len())
+
+		case tar.TypeDir:
+			fmt.Printf("dir filename %s\n", header.Name)
+		}
+	}
 }
